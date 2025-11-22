0bd76c6046c17a4f00b7a8bf7338aa2dbe49cc0d
SamarthArun12 <samarth.arun.ca@gmail.com>
2025-11-21 23:02:44 -0800

    Update entity_class.py

--- entity_class.py
+++ entity_class.py
@@ -1,7 +1,10 @@
 class entity:
-    def __init__(self, health, skills):
+    def __init__(self, health, skills, x, y):
+        self.max_hp = health
         self.hp = health
         self.skills = skills
+        self.x = x
+        self.y = y
     
     def get_skills(self):
         return self.skills
@@ -12,4 +15,10 @@ class entity:
     def take_damage(self, damage):
         self.hp -= damage
         return self.hp
+    
+    def get_hp(self):
+        return self.hp
+    
+    def get_pos(self):
+        return self.x, self.y
     
